adefd1c4b7996d683c47762568fa6072aa632722
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-05-03 21:48:48 -0400

    added classes for teh shapes. fixed several bugs involving the version of inquirer giving errors. finalized functions to create svg files.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// packages 
 const inquirer = require("inquirer");
+
+
 const fs = require("fs");
-const { Circle, Square, Triangle } = require("./shapes");
+const Circle = require('./lib/circle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
 
 
-
-
-
-
-
-
-inquirer.prompt([
+const questions = [
     {
         name: "characters",
         type: "input",
@@ -37,7 +34,44 @@ inquirer.prompt([
         "Input a color for your shape (example: `green` OR `#2BA404`):",
         validate: (value) => colorCheck(value) ? true : 'Please enter a color.'
     },
-]);
+];
+
+function logoQuestions() {
+    return inquirer.prompt(questions);
+};
+
+function createLogo(answers) {
+    const { characters, textColor, shape, shapeColor } = answers;
+    let shapeRender = '';
+
+    // Render text SVG
+    const logoText = `<text x="150" y="105" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-size="45">${characters}</text>`;
+
+    // Render shape SVG based on user's choice
+    switch (shape) {
+        case 'Circle':
+            const circle = new Circle(shapeColor);
+            shapeRender = circle.render();
+            break;
+        case 'Square':
+            const square = new Square(shapeColor);
+            shapeRender = square.render();
+            break;
+        case 'Triangle':
+            const triangle = new Triangle(shapeColor);
+            shapeRender = triangle.render();
+            break;
+    }
+
+    // Combine text and shape SVG into final logo SVG
+    const svg = `<svg width="300" height="250" xmlns="http://www.w3.org/2000/svg">
+    ${shapeRender}
+    ${logoText}
+</svg>`;
+
+    return svg;
+}
+
 
 // Function to check color values
 function colorCheck(value) {
@@ -45,3 +79,18 @@ function colorCheck(value) {
     let colorHex = new RegExp(/^#[A-Fa-f0-9]{6}$/);
     return colorName.test(value) || colorHex.test(value);
 };
+
+
+
+logoQuestions().then((answers) => {
+    const logo = createLogo(answers);
+    const resultPath = `./examples/logo.svg`;
+
+    fs.writeFile(resultPath, logo, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Your logo has been created!');
+    })
+});
